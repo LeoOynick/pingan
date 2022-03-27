e1ef68442550d24f61e267eba808d9376059f6f7
WZHabc <101922245+WZHabc@users.noreply.github.com>
2022-03-27 16:48:51 +0800

    。

--- source/CARSERVICE.c
+++ source/CARSERVICE.c
@@ -1,14 +1,208 @@
 #include"common.h"
 #include"carservice.h"
 
-void carserve()
+void carserve(int *page)
 {
-	int gdriver=VGA;
-    int gmode=VGAHI;
-    initgraph(&gdriver,&gmode,"C:\\BORLANDC\\BGI");
+	int num = 0;
+	int state1 = 0;
+	int state2 = 0;
+	int state3 = 0;
+	int state4 = 0;
 	
+	clrmous(MouseX, MouseY);
+	delay(100);
+	cleardevice();
+	drawcarserve();
+	
+	while(1)
+	{
+		newmouse(&MouseX,&MouseY,&press);
+		if(mouse_press(610,0,640,30) == 2)   //退出
+		{
+			if (num == 0)
+		    {
+			    MouseS = 1;
+				clrmous(MouseX, MouseY);
+				delay(10);
+				setfillstyle(1,RED);
+				bar(610,0,640,30);
+			    setcolor(15);
+				setlinestyle(0,4,3);
+			    line(610,0,640,30);
+				line(640,0,610,30);
+			    num = 1;
+		    }
+			continue;
+		}
+		else if(mouse_press(610,0,640,30) == 1)
+		{
+			*page = 1;
+			return;
+		}
+		
+		else if(mouse_press(180,400,260,430) == 2)   //确定
+		{
+			if (num == 0)
+			{
+				MouseS = 1;
+				clrmous(MouseX, MouseY);
+				delay(10);
+				button(180,400,260,430,15,10,1);
+				puthz(195,405,"确定",24,28,1);
+				num = 2;
+			}
+			continue;
+		}
+		else if (mouse_press(180,400,260,430) == 1)
+		{
+			MouseS = 0;
+			
+		}
+		
+		else if(mouse_press(380,400,460,430) == 2)   //返回
+		{
+			if (num == 0)
+			{
+				MouseS = 1;
+				clrmous(MouseX, MouseY);
+				delay(10);
+				button(380,400,460,430,15,12,1);
+				puthz(395,405,"返回",24,28,1);
+				num = 3;
+			}
+			continue;
+		}
+		else if (mouse_press(380,400,460,430) == 1)
+		{
+			MouseS = 0;
+		    *page = 7;
+			return;
+		}
+		
+		else if (mouse_press(520,135,600,155) == 2)   //切换车辆
+		{
+			if (num == 0)
+			{
+				MouseS = 1;
+				clrmous(MouseX, MouseY);
+				delay(10);
+				num = 4;
+			}
+			continue;
+		}
+		else if (mouse_press(520,135,600,155) == 1)
+		{
+			MouseS = 0;
+		    //*page = 6;
+			return;
+		}
+		
+		else if (mouse_press(95,290,105,300) == 2)   //洗车
+		{
+			if (num == 0)
+			{
+				MouseS = 1;
+				clrmous(MouseX, MouseY);
+				delay(10);
+				num = 5;
+			}
+			continue;
+		}
+		else if (mouse_press(95,290,105,300) == 1)
+		{
+			delay(150);
+			choose(100,295,state1);
+		}
+		
+		else if (mouse_press(245,290,255,300) == 2)   //加油
+		{
+			if (num == 0)
+			{
+				MouseS = 1;
+				clrmous(MouseX, MouseY);
+				delay(10);
+				num = 6;
+			}
+			continue;
+		}
+		else if (mouse_press(245,290,255,300) == 1)
+		{
+			delay(150);
+			choose(250,295,state2);
+		}
+		
+		else if (mouse_press(395,290,405,300) == 2)   //保养
+		{
+			if (num == 0)
+			{
+				MouseS = 1;
+				clrmous(MouseX, MouseY);
+				delay(10);
+				num = 7;
+			}
+			continue;
+		}
+		else if (mouse_press(395,290,405,300) == 1)
+		{
+			delay(150);
+			choose(400,295,state3);
+		}
+		
+		else if (mouse_press(545,290,555,300) == 2)   //挪车
+		{
+			if (num == 0)
+			{
+				MouseS = 1;
+				clrmous(MouseX, MouseY);
+				delay(10);
+				num = 8;
+			}
+			continue;
+		}
+		else if (mouse_press(545,290,555,300) == 1)
+		{
+			delay(150);
+			choose(550,295,state4);
+		}
+		
+		else
+		{
+			if(num != 0)
+			{
+				MouseS = 0;
+				clrmous(MouseX, MouseY);
+				delay(10);
+				if(num == 1)
+				{
+					setfillstyle(1,LIGHTGRAY);
+					bar(610,0,640,30);
+					setcolor(1);
+					setlinestyle(0,4,3);
+					line(610,0,640,30);
+					line(640,0,610,30);
+				}
+				else if(num == 2)
+				{
+					setfillstyle(1,2);
+					bar(180,400,260,430);
+					puthz(195,405,"确定",24,28,1);
+				}
+				else if(num == 3)
+				{
+					setfillstyle(1,4);
+					bar(380,400,460,430);
+					puthz(395,405,"返回",24,28,1);
+				}
+				num = 0;
+			}
+			continue;
+		}
+	}
+}
+
+void drawcarserve()
+{
 	setbkcolor(LIGHTCYAN);
-	
 	puthz(220,20,"车辆服务",48,56,1);
 	puthz(20,120,"车辆信息",24,28,1);
 	puthz(520,135,"切换车辆",16,18,8);
@@ -38,9 +232,9 @@ void carserve()
 	
 	puthz(160,330,"共计消费：",24,28,1);
 	puthz(400,335,"元",16,18,8);
-	setfillstyle(1,12);
+	setfillstyle(1,GREEN);
 	bar(180,400,260,430);
-	setfillstyle(1,14);
+	setfillstyle(1,4);
 	bar(380,400,460,430);
 	puthz(195,405,"确定",24,28,1);
 	puthz(395,405,"返回",24,28,1);
--- source/INSPECTION.c
+++ source/INSPECTION.c
@@ -1,13 +1,172 @@
 #include"common.h"
+#include"inspection.h"
 
-void inspect()
+void inspect(int *page)
 {
-	int gdriver=VGA;
-    int gmode=VGAHI;
-    initgraph(&gdriver,&gmode,"C:\\BORLANDC\\BGI");
+	int num = 0;
+	int state1 = 0;
+	int state2 = 0;
 	
-	setbkcolor(LIGHTCYAN);
+	clrmous(MouseX, MouseY);
+	delay(100);
+	cleardevice();
+	drawinspect();
 	
+	while(1)
+	{
+		newmouse(&MouseX,&MouseY,&press);
+		if(mouse_press(610,0,640,30) == 2)   //退出
+		{
+			if (num == 0)
+		    {
+			    MouseS = 1;
+				clrmous(MouseX, MouseY);
+				delay(10);
+				setfillstyle(1,RED);
+				bar(610,0,640,30);
+			    setcolor(15);
+				setlinestyle(0,4,3);
+			    line(610,0,640,30);
+				line(640,0,610,30);
+			    num = 1;
+		    }
+			continue;
+		}
+		else if(mouse_press(610,0,640,30) == 1)
+		{
+			*page = 1;
+			return;
+		}
+		
+		else if(mouse_press(180,400,260,430) == 2)   //确定
+		{
+			if (num == 0)
+			{
+				MouseS = 1;
+				clrmous(MouseX, MouseY);
+				delay(10);
+				button(180,400,260,430,15,10,1);
+				puthz(195,405,"确定",24,28,1);
+				num = 2;
+			}
+			continue;
+		}
+		else if (mouse_press(180,400,260,430) == 1)
+		{
+			MouseS = 0;
+			
+		}
+		
+		else if(mouse_press(380,400,460,430) == 2)   //返回
+		{
+			if (num == 0)
+			{
+				MouseS = 1;
+				clrmous(MouseX, MouseY);
+				delay(10);
+				button(380,400,460,430,15,12,1);
+				puthz(395,405,"返回",24,28,1);
+				num = 3;
+			}
+			continue;
+		}
+		else if (mouse_press(380,400,460,430) == 1)
+		{
+			MouseS = 0;
+		    *page = 7;
+			return;
+		}
+		
+		else if (mouse_press(520,135,600,155) == 2)   //切换车辆
+		{
+			if (num == 0)
+			{
+				MouseS = 1;
+				clrmous(MouseX, MouseY);
+				delay(10);
+				num = 4;
+			}
+			continue;
+		}
+		else if (mouse_press(520,135,600,155) == 1)
+		{
+			MouseS = 0;
+		    //*page = 6;
+			return;
+		}
+		
+		else if (mouse_press(175,305,185,315) == 2)   //新车免检（免上线）
+		{
+			if (num == 0)
+			{
+				MouseS = 1;
+				clrmous(MouseX, MouseY);
+				delay(10);
+				num = 5;
+			}
+			continue;
+		}
+		else if (mouse_press(175,305,185,315) == 1)
+		{
+			delay(150);
+			choose(180,310,state1);
+		}
+		
+		else if (mouse_press(455,305,465,315) == 2)   //上检测站年检（上线）
+		{
+			if (num == 0)
+			{
+				MouseS = 1;
+				clrmous(MouseX, MouseY);
+				delay(10);
+				num = 6;
+			}
+			continue;
+		}
+		else if (mouse_press(455,305,465,315) == 1)
+		{
+			delay(150);
+			choose(460,310,state2);
+		}
+		
+		else
+		{
+			if(num != 0)
+			{
+				MouseS = 0;
+				clrmous(MouseX, MouseY);
+				delay(10);
+				if(num == 1)
+				{
+					setfillstyle(1,LIGHTGRAY);
+					bar(610,0,640,30);
+					setcolor(1);
+					setlinestyle(0,4,3);
+					line(610,0,640,30);
+					line(640,0,610,30);
+				}
+				else if(num == 2)
+				{
+					setfillstyle(1,2);
+					bar(180,400,260,430);
+					puthz(195,405,"确定",24,28,1);
+				}
+				else if(num == 3)
+				{
+					setfillstyle(1,4);
+					bar(380,400,460,430);
+					puthz(395,405,"返回",24,28,1);
+				}
+				num = 0;
+			}
+			continue;
+		}
+	}
+}
+
+void drawinspect()
+{
+	setbkcolor(LIGHTCYAN);	
 	puthz(220,20,"年检代办",48,56,1);
 	puthz(20,120,"车辆信息",24,28,1);
 	puthz(520,135,"切换车辆",16,18,8);
@@ -31,9 +190,9 @@ void inspect()
 	
 	puthz(160,350,"共计消费：",24,28,1);
 	puthz(400,355,"元",16,18,8);
-	setfillstyle(1,12);
+	setfillstyle(1,GREEN);
 	bar(180,400,260,430);
-	setfillstyle(1,14);
+	setfillstyle(1,4);
 	bar(380,400,460,430);
 	puthz(195,405,"确定",24,28,1);
 	puthz(395,405,"返回",24,28,1);
